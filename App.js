874963dd2f7320441003f271f59de742f85cf41b
darrynrandall <darryn.randall@gmail.com>
2025-08-04 21:51:19 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,1 +1,12 @@
-// Your updated App.js goes here
+import React from 'react';
+import './theme.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello from Everdell Randomizer!</h1>
+    </div>
+  );
+}
+
+export default App;
